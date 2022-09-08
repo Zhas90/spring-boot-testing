bb27d6538b60da4c3a215850e012e221ca4eb039
Zhassulan_Baimanov <zh.baimanov@gmail.com>
2022-09-08 16:05:09 +0600

    refactoring

--- src/test/java/kz/zhassulan/springboottesting/controller/EmployeeControllerTests.java
+++ src/test/java/kz/zhassulan/springboottesting/controller/EmployeeControllerTests.java
@@ -3,26 +3,25 @@ package kz.zhassulan.springboottesting.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kz.zhassulan.springboottesting.model.Employee;
 import kz.zhassulan.springboottesting.service.EmployeeService;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 public class EmployeeControllerTests {
@@ -45,7 +44,7 @@ public class EmployeeControllerTests {
                 .lastName("Usen")
                 .email("asan@gmail.com")
                 .build();
-        given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
+        given(employeeService.saveEmployee(any(Employee.class)))
                 .willAnswer((invocation) -> invocation.getArgument(0));
 
         //when - action or the behaviour that we are going to test
@@ -54,14 +53,14 @@ public class EmployeeControllerTests {
                 .content(objectMapper.writeValueAsBytes(employee)));
 
         //then - verify the output
-        response.andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName",
-                        CoreMatchers.is(employee.getFirstName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName",
-                        CoreMatchers.is(employee.getLastName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.email",
-                        CoreMatchers.is(employee.getEmail())));
+        response.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName",
+                        is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName",
+                        is(employee.getLastName())))
+                .andExpect(jsonPath("$.email",
+                        is(employee.getEmail())));
     }
 
     @Test
@@ -78,9 +77,9 @@ public class EmployeeControllerTests {
         ResultActions response = mockMvc.perform(get("/api/employees"));
 
         //then - verify the output
-        response.andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(listOfEmployees.size())));
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(listOfEmployees.size())));
     }
 
 }
